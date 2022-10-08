2f7306e37da8ca6a484ce945295eb26bb35c6106
12138 <41439182+Drelf2018@users.noreply.github.com>
2022-10-08 16:06:31 +0800

    Update index.py

--- code/index.py
+++ code/index.py
@@ -1,39 +1,42 @@
 import uvicorn
+import httpx
+from lxml import etree
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
+@app.get('/get_list')
+async def get_list():
+    data = etree.HTML(httpx.get('https://acrnm.com/?sort=default&filter=txt').text)
+    Products = dict()
+    for tr in data.xpath('.//tbody/tr'):
+        for td in tr.xpath('./td'):
+            match td.xpath("./@class")[0][39:-5]:
+                case "title":
+                    name = td.xpath('.//span/text()')[0]
+                    if name not in Products:
+                        Products[name] = dict()
+                        Products[name]["price"] = dict()
+                case "variant":
+                    variant = dict()
+                    for span in td.xpath('./div/span'):
+                        color = '/'.join(span.xpath("./div/span/text()"))
+                        size = '/'.join(span.xpath("./span/text()"))       
+                        variant[color] = size
+                case "price":
+                    if val := td.xpath('.//span/text()'):
+                        if val[0] != "0.00 EUR":
+                            Products[name]["price"][val[0]] = variant
 
-@app.get('/')
-async def hello():
-    return HTMLResponse('''<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <title>Serverless Devs - Powered By Serverless Devs</title>
-    <link href="https://example-static.oss-cn-beijing.aliyuncs.com/web-framework/style.css" rel="stylesheet" type="text/css"/>
-</head>
-<body>
-<div class="website">
-    <div class="ri-t">
-        <h1>Devsapp</h1>
-        <h2>这是一个 FastAPI 项目</h2>
-        <span>自豪的通过Serverless Devs进行部署</span>
-        <br/>
-        <p>您也可以快速体验： <br/>
-            • 下载Serverless Devs工具：npm install @serverless-devs/s<br/>
-            • 初始化项目：s init start-fastapi<br/>
+    imgs = etree.HTML(httpx.get("https://acrnm.com/").text)
+    for name in Products:
+        try:
+            img = "https://acrnm.com/" + imgs.xpath(f'.//span[text()="{name}"]/../img/@src')[0]
+            Products[name]['img'] = img
+        except:
+            ...
 
-            • 项目部署：s deploy<br/>
-            <br/>
-            Serverless Devs 钉钉交流群：33947367
-        </p>
-    </div>
-</div>
-</body>
-</html>
-''')
-
+    return Products
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=9000)
