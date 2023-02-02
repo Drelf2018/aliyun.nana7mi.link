cd95fab72ab5949715048de15536082ac173a060
Drelf2018 <drelf2018@outlook.com>
2023-02-03 03:45:01 +0800

    BREAKING CHANGE: 把这坨狗屎函数搞得更狗屎了
    用法估计只有我看得懂，，，等我有空了就写教程

--- code/index.py
+++ code/index.py
@@ -1,11 +1,13 @@
 import re
+from enum import Enum
 from inspect import iscoroutinefunction as isAsync, isfunction as isFn, isclass
-from typing import List
+from typing import List, Tuple
 
 import bilibili_api
 import httpx
 import uvicorn
 from fastapi import FastAPI, Response
+from fastapi.responses import FileResponse
 from lxml import etree
 
 # weibo.cn COOKIES
@@ -18,6 +20,7 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36',
     'cookie': '_T_WM=4f424bc33be0d62a2d75deaea7663a7e; SUB=_2A25OYyC6DeRhGeFJ6FoX8SjNzjqIHXVtrEDyrDV6PUJbkdAKLUTYkW1NfB9c4mX8iDOuMuRJjRhusVSksNkPl5Az; SCF=AjTyISCkRlwIyYTl4s8fHOI4utjEYr3jQbVCaQ3YIXWD4Bm52DNP72ago_zCTbpsLVpzwTBINAhovOAq7oM4owc.; SSOLoginState=1667715306'
 }
+pattern = re.compile(r'(?:([:\$\w]+(?:=\w+)?),?)')
 app = FastAPI()
 
 @app.get("/weibo/{uid}")
@@ -52,46 +55,97 @@ def getLoginInfo(oauthKey: str):
     else:
         return {'DedeUserID': -1}
 
+@app.get("/favicon.ico")
+async def favicon():
+    return FileResponse("favicon.ico")
+
+class Parser:
+    def __init__(self, var: str):
+        self.valid = True
+        self.varDict = dict(v.split("<") for v in var.split(",")) if var else dict()
+
+    async def __aenter__(self):
+        for key in list(self.varDict.keys()):
+            obj, err = await self.parse(self.varDict[key])
+            if not err:
+                if isinstance(obj, bilibili_api.Credential):
+                    self.valid = await obj.check_valid()
+                self.varDict["$"+key] = obj
+        return self
+
+    async def __aexit__(self, type, value, trace): ...
+
+    async def parse(self, path: str) -> Tuple[any, bool]:
+        "分析指令"
+
+        sentences = path.split(".")  # 指令列表
+        position: any = bilibili_api  # 起始点
+
+        async def inner() -> bool:
+            "递归取值"
+
+            nonlocal position
+            if len(sentences) == 0:
+                return position is None  # 判断是否取得具体对象
+
+            sentence = sentences.pop(0)
+            # 分解执行的函数名、参数、指名参数
+            flags: List[str] = pattern.findall(sentence)
+            func = flags.pop(0)
+            args, kwargs = list(), dict()
+
+            for flag in flags:
+                # 假设分为键值形式 利用列表特性从 -1 读取值
+                # 即使没有键也能读到值
+                arg = flag.split("=")
+                # 类型装换
+                if arg[-1].endswith(":int"):
+                    arg[-1] = int(arg[-1][:-4])
+                # 将值与储存的变量替换
+                arg[-1] = self.varDict.get(arg[-1], arg[-1])
+                # 存入对应的参数、指名参数
+                if len(arg) == 1:
+                    args.append(arg[0])
+                else:
+                    kwargs[arg[0]] = arg[1]
+
+            # 开始转移
+            if isinstance(position, dict):
+                position = position.get(func, None)
+            else:
+                position = getattr(position, func, None)
+
+            # 赋值参数
+            if isAsync(position):
+                position = await position(*args, **kwargs)
+            elif isFn(position):
+                position = position(*args, **kwargs)
+            elif isclass(position) and not issubclass(position, Enum):
+                position = position(*args, **kwargs)
+
+            # 递归
+            return await inner()
+
+        err = await inner()
+        return position, err
+
+
 @app.get("/{path}")
-async def bilibili_api_web(
-    response: Response,
-    path: str,
-    SESSDATA: str = None,
-    bili_jct: str = None,
-    buvid3: str = None,
-    DedeUserID: str = None,
-    max_age: int = -1
-):
+async def bilibili_api_web(response: Response, path: str, var: str = "", max_age: int = -1):
+    # 返回头设置
     response.headers["Access-Control-Allow-Origin"] = "*"
-    if max_age != -1: response.headers["Cache-Control"] = f"max-age={max_age}"
-    credential = bilibili_api.Credential(
-        sessdata=SESSDATA,
-        bili_jct=bili_jct,
-        buvid3=buvid3,
-        dedeuserid=DedeUserID
-    )
-    if any([SESSDATA, bili_jct, buvid3, DedeUserID]):
-        if await credential.check_valid() is False:
+    if max_age != -1:
+        response.headers["Cache-Control"] = f"max-age={max_age}"
+    
+    # 先判断是否有效 再分析
+    async with Parser(var) as parser:
+        if not parser.valid:
             return {"code": 1, "error": "Cookies Error"}
-    position = bilibili_api
-    pattern = re.compile(r'(?:(\w+(?:=\w+)?),?)')
-    for sentence in path.split("."):
-        flags: List[str] = pattern.findall(sentence)
-        func = flags.pop(0)
-        args = tuple(arg for arg in flags if "=" not in arg)
-        kwargs = dict(arg.split("=") for arg in flags if "=" in arg)
-        if "credential" in kwargs:
-            kwargs["credential"] = credential
-        position = position.get(func, None) if isinstance(position, dict) else getattr(position, func, None)
-        if isAsync(position):
-            position = await position(*args, **kwargs)
-        elif isFn(position) or isclass(position):
-            position = position(*args, **kwargs)
-        if position is None:
-            break
-    else:
-        return {"code": 0, "data": position}
-    return {"code": 2, "error": "Path Error"}
+        obj, err = await parser.parse(path)  # 什么 golang 写法
+        if not err:
+            return {"code": 0, "data": obj}
+        else:
+            return {"code": 2, "error": "Path Error"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=9000)
