29c562a9ed4e7b898191cbed1ea7b0827691886c
Drelf2018 <drelf2018@outlook.com>
2023-03-03 21:27:44 +0800

    feat: 新建分析函数

--- code/index.py
+++ code/index.py
@@ -3,12 +3,19 @@ import uvicorn
 from bilibili_api.tools.parser import get_fastapi
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from weibo_poster import get_content, parse_text
 
-fastapp = FastAPI()
-fastapp.get("/favicon.ico")(lambda: FileResponse("favicon.ico"))
-fastapp.get("/getLoginUrl")(lambda: httpx.get("https://passport.bilibili.com/qrcode/getLoginUrl").json())
+app = FastAPI()
 
-@fastapp.get("/getLoginInfo")
+@app.get("/favicon.ico")
+def favicon():
+    return FileResponse("favicon.ico")
+
+@app.get("/getLoginUrl")
+def getLoginUrl():
+    return httpx.get("https://passport.bilibili.com/qrcode/getLoginUrl").json()
+
+@app.get("/getLoginInfo")
 def getLoginInfo(oauthKey: str):
     js = httpx.post('https://passport.bilibili.com/qrcode/getLoginInfo', data={'oauthKey': oauthKey}).json()
     if js['status']:
@@ -19,7 +26,12 @@ def getLoginInfo(oauthKey: str):
     else:
         return {'DedeUserID': -1}
 
-fastapp.mount("/", get_fastapi())
+@app.get("/content")
+def content(text: str):
+    ts, _ = parse_text(text)
+    return get_content(ts)
+
+app.mount("/", get_fastapi())
 
 if __name__ == "__main__":
-    uvicorn.run(fastapp, host="0.0.0.0", port=9000)
+    uvicorn.run(app, host="0.0.0.0", port=9000)
